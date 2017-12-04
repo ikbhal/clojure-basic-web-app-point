ec69c5917d0559a601614bc17c18683eb1b5ec11
Ubuntu <ubuntu@ip-172-31-7-243.ap-south-1.compute.internal>
2017-12-04 16:56:02 +0000

    Completed tutorial

--- src/my_webapp/handler.clj
+++ src/my_webapp/handler.clj
@@ -1,10 +1,26 @@
 (ns my-webapp.handler
-  (:require [compojure.core :refer :all]
+  (:require [my-webapp.views :as views] ; add this require
+            [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/"
+       []
+       (views/home-page))
+  (GET "/add-location"
+       []
+       (views/add-location-page))
+  (POST "/add-location"
+       {params :params}
+       (views/add-location-results-page params))
+  (GET "/location/:loc-id"
+       [loc-id]
+       (views/location-page loc-id))
+  (GET "/all-locations"
+       []
+       (views/all-locations-page))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
